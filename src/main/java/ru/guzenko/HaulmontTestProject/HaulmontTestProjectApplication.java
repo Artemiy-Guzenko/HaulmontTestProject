b8d63dff2d6cdd9988398ffdac13db1e54038268
artem <artemiyguzenko@gmail.com>
2021-01-17 22:54:41 +0400

    Initial commit

---
+++ src/main/java/ru/guzenko/HaulmontTestProject/HaulmontTestProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.guzenko.HaulmontTestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaulmontTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaulmontTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/guzenko/HaulmontTestProject/HaulmontTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.guzenko.HaulmontTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulmontTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
